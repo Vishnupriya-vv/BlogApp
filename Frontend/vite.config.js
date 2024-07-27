87d5d251f94d217e1206f2969275fa8238106720
Vishnupriya V <vsnpia@gmai.com>
2024-07-27 13:59:40 +0530

    third commit

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Bundle all node_modules into a single chunk
+          }
+          // You can add more custom chunking logic here if needed
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Adjust chunk size warning limit to 1000 kB
+  },
 })
